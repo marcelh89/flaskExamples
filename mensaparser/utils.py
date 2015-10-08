5745b9d867dc6ca713d2897ad2ca61a5deef2d0f
MaRcMaN <marcelh89@googlemail.com>
2015-10-08 17:42:36 +0200

    heutiges essen

--- mensaparser/utils.py
+++ mensaparser/utils.py
@@ -6,20 +6,25 @@ from bs4 import BeautifulSoup
 
 def get_api_as_json():
     URL = 'http://www.studentenwerk-potsdam.de/speiseplan/'
-
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, "html.parser")
-
     thisweek_meals = get_meals(soup, 'c16814')
     nextweek_meals = get_meals(soup, 'c16815')
 
+    URL_today = 'http://www.studentenwerk-potsdam.de/mensa-brandenburg.html'
+    r_today = requests.get(URL_today)
+    soup_today = BeautifulSoup(r_today.text, "html.parser")
+    today_meals = get_todaysmeals(soup_today, 'c10856')
+
     data = {}
     data['date'] = str(datetime.datetime.now())
-    data['meals'] = thisweek_meals + nextweek_meals
+    data['meals'] = today_meals + thisweek_meals + nextweek_meals
 
     return data
 
-
+'''
+    wird verwendet fuer alle tage ab morgen
+'''
 def get_meals(soup, id):
     container = soup.find("div", {"id": id})
     tables = container.find_all("table", {'class': 'bill_of_fare'})
@@ -28,7 +33,9 @@ def get_meals(soup, id):
 
     return meals
 
-
+'''
+    wird verwendet fuer alle tage ab morgen
+'''
 def get_meal(table):
     rows = table.find_all('tr')
     date = rows[0].text
@@ -44,3 +51,37 @@ def get_meal(table):
     }
 
     return meal
+
+'''
+    wird verwendet fuer heute
+'''
+def get_todaysmeals(soup, id):
+    container = soup.find("div", {"id": id})
+    tables = container.find_all("table", {'class': 'bill_of_fare'})
+
+    meals = [get_todaysmeal(table, container) for table in tables]
+
+    return meals
+
+'''
+    wird verwendet fuer heute
+'''
+def get_todaysmeal(table, container):
+    rows = table.find_all('tr')
+    date = container.find('h2', {'id': 'ueberschrift_h2'})
+    date = str(date.text)
+    date = date[date.find('den') + 3:].strip()
+
+    headings = [col.text for col in rows[0] if col != '\n']
+    meals = [col.text for col in rows[1] if col != '\n']
+    icons = [list(img.get('alt', '') for img in col.find_all('img')) for col in
+             rows[2] if col != '\n']
+
+    meal = {
+        'date': date,
+        'meals': [{'heading': heading, 'meal': meal, 'icon': icon} for heading, meal, icon in
+                  zip(headings, meals, icons)]
+    }
+
+    return meal
+
